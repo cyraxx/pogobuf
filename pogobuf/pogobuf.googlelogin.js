21b8c8bda79694f586fed54c10beceaed955d0cc
Uluc Sengil <usengil@ku.edu.tr>
2016-07-31 17:15:35 -0700

    Added a new method in googlelogin.js to enable logging in with an existing token

--- pogobuf/pogobuf.googlelogin.js
+++ pogobuf/pogobuf.googlelogin.js
@@ -41,6 +41,20 @@ function GoogleLogin() {
     };
 
     /**
+     * Performs the Google login by skipping the password step and starting with the Master Token
+     * instead.
+     * @param {string} username
+     * @param {string} token
+     * @return {Promise}
+     */
+     this.loginWithToken = function(username, token) {
+        var loginData = { androidId: GOOGLE_LOGIN_ANDROID_ID, masterToken: token };
+        return self.getToken(username, loginData).then((authData) => {
+            return authData.Auth;
+        });
+     };
+
+    /**
      * Initialize Google Login
      * @param {string} username
      * @param {string} password
