43f57adc17be87cb8ff36d775a9cbfb57cf426fa
Andreas Reich <andreas@reich.name>
2017-01-04 22:15:18 +0100

    :books: Fix wrong method name in JSDoc

--- pogobuf/pogobuf.client.js
+++ pogobuf/pogobuf.client.js
@@ -64,7 +64,7 @@ function Client(options) {
      * Sets the player's latitude and longitude.
      * Note that this does not actually update the player location on the server, it only sets
      * the location to be used in following API calls. To update the location on the server you
-     * probably want to call {@link #updatePlayer}.
+     * probably want to call {@link #playerUpdate}.
      * @param {number|object} latitude - The player's latitude, or an object with parameters
      * @param {number} longitude - The player's longitude
      * @param {number} [accuracy=0] - The location accuracy in m
