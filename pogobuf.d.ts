93a77d3d15c748ccff64c92b946e510c0ae3cae5
nico <nicolas.schmitt@outlook.fr>
2017-02-19 09:26:45 +0100

    remove playerupdate from typings

--- pogobuf.d.ts
+++ pogobuf.d.ts
@@ -235,9 +235,6 @@ declare namespace pogobuf {
             nickname: string
         ): Promise<POGOProtos.Networking.Responses.NicknamePokemonResponse>;
 
-        playerUpdate(
-        ): Promise<POGOProtos.Networking.Responses.PlayerUpdateResponse>;
-
         registerBackgroundDevice(
             deviceType: string,
             deviceID: string
