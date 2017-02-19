94944c6d1181b98560ddd862657273d7c168d963
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-02-19 21:03:50 +0100

    Hash server rate infos

--- pogobuf/pogobuf.client.js
+++ pogobuf/pogobuf.client.js
@@ -167,7 +167,7 @@ function Client(options) {
      * @return {Object}
      */
     this.getSignatureRateInfo = function() {
-        return self.signatureBuilder.utils.rateInfos;
+        return self.signatureBuilder.rateInfos;
     };
 
     /*
